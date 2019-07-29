84b7565562593249d789a4c920f25de5b7c57eb7
chintan <chintan.adatiya@gmail.com>
2019-07-29 15:51:21 +0530

    test commit to check auto deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React Autodeploy with zeit
+          React Autodeploy with zeit, TEST
         </p>
       </header>
     </div>
